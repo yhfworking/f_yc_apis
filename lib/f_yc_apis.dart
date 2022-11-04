b14198221cb8c388a78decc46e1aa76073e3cb0e
yhfwork <mm20102010mm>
2022-11-04 22:23:07 +0800

    Update f_yc_apis.dart

--- lib/f_yc_apis.dart
+++ lib/f_yc_apis.dart
@@ -3,11 +3,14 @@ library f_yc_apis;
 export 'src/provider/f_yc_apis_provider.dart';
 export 'src/apis/yc_apis_auth.dart';
 export 'src/apis/yc_apis_default.dart';
+import 'dart:async';
+import 'package:f_yc_utils/f_yc_utils.dart';
 import 'src/provider/f_yc_apis_provider.dart';
 
 class FYcApi {
   static FYcApisProvider apisProvider = FYcApisProvider();
-  static initializer() async {
+  static initializer(FutureOr<Request> requestInterceptor,
+      FutureOr<dynamic> responseInterceptor) async {
     apisProvider = FYcApisProvider();
   }
 }
