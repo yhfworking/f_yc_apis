2c05810a320837fb1833b3433be3f9d67b4c80fb
yhfwork <mm20102010mm>
2022-11-05 22:07:01 +0800

    Update f_yc_apis.dart

--- lib/f_yc_apis.dart
+++ lib/f_yc_apis.dart
@@ -1,6 +1,7 @@
 library f_yc_apis;
 
 export './src/f_yc_apis_dio.dart';
+export './src/f_yc_apis_ base_response.dart';
 // export 'src/provider/f_yc_apis_provider.dart';
 // export 'src/apis/yc_apis_auth.dart';
 // export 'src/apis/yc_apis_default.dart';
