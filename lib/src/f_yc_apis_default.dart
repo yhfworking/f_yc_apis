10e70c0eb0617c6dc3d35ee403dee281f7e03f9c
yhfwork <mm20102010mm>
2022-11-09 02:18:50 +0800

    update

---
+++ lib/src/f_yc_apis_default.dart
@@ -0,0 +1,270 @@
+import 'package:f_yc_apis/src/f_yc_apis_dio.dart';
+import 'package:f_yc_entity/f_yc_entity.dart';
+import 'package:f_yc_storages/f_yc_storages.dart';
+import 'package:f_yc_utils/f_yc_utils.dart';
+import 'f_yc_apis_ base_response.dart';
+
+class FYcApisDefault {
+//   static Future<Map<String, dynamic>?> wxLogin(String uri, String code,
+//       {String inviteCode = ''}) async {
+//     FYcApisBaseResponse apisBaseResponse = await FYcApisDio.instance
+//         .post('/api/default/pub_remoteConfig.query', params: {}, tips: true);
+//     if (apisBaseResponse.success) {
+//       FYcStorages.setUserToken(apisBaseResponse.data['token']);
+//       FYcStorages.setUserTokenExpired(apisBaseResponse.data['tokenExpired']);
+
+//       FYcStorages.setUserInfo(
+//           FYcEntitysUser.fromJson(apisBaseResponse.data['userInfo'] ?? {}));
+//       FYcStorages.setBehaviorInfo(FYcEntitysBehavior.fromJson(
+//           apisBaseResponse.data['behaviorInfo'] ?? {}));
+//       FYcStorages.setWalletInfo(
+//           FYcEntitysWallet.fromJson(apisBaseResponse.data['walletInfo'] ?? {}));
+//       FYcEventBus.instance.fire(FYcEntitysEventsUserInfoUpdate());
+//       FYcEventBus.instance.fire(FYcEntitysEventsBehaviorUpdate());
+//       FYcEventBus.instance.fire(FYcEntitysEventsWalletUpdate());
+//     }
+//     return apisBaseResponse.data;
+//   }
+
+//   static Future<Map<String, dynamic>?> mLogin(String uri) async {
+//     FYcApisBaseResponse apisBaseResponse = await FYcApisDio.instance
+//         .post('/api/default/pub_remoteConfig.query', params: {}, tips: true);
+//     if (apisBaseResponse.success) {
+//       FYcStorages.setUserToken(apisBaseResponse.data['token']);
+//       FYcStorages.setUserTokenExpired(apisBaseResponse.data['tokenExpired']);
+
+//       FYcStorages.setUserInfo(
+//           FYcEntitysUser.fromJson(apisBaseResponse.data['userInfo'] ?? {}));
+//       FYcStorages.setBehaviorInfo(FYcEntitysBehavior.fromJson(
+//           apisBaseResponse.data['behaviorInfo'] ?? {}));
+//       FYcStorages.setWalletInfo(
+//           FYcEntitysWallet.fromJson(apisBaseResponse.data['walletInfo'] ?? {}));
+//       FYcEventBus.instance.fire(FYcEntitysEventsUserInfoUpdate());
+//       FYcEventBus.instance.fire(FYcEntitysEventsBehaviorUpdate());
+//       FYcEventBus.instance.fire(FYcEntitysEventsWalletUpdate());
+//     }
+//     return apisBaseResponse.data;
+//   }
+
+//   static Future<Map<String, dynamic>?> appleLogin(String uri) async {
+//     FYcApisBaseResponse apisBaseResponse = await FYcApisDio.instance
+//         .post('/api/default/pub_remoteConfig.query', params: {}, tips: true);
+//     if (apisBaseResponse.success) {
+//       FYcStorages.setUserToken(apisBaseResponse.data['token']);
+//       FYcStorages.setUserTokenExpired(apisBaseResponse.data['tokenExpired']);
+
+//       FYcStorages.setUserInfo(
+//           FYcEntitysUser.fromJson(apisBaseResponse.data['userInfo'] ?? {}));
+//       FYcStorages.setBehaviorInfo(FYcEntitysBehavior.fromJson(
+//           apisBaseResponse.data['behaviorInfo'] ?? {}));
+//       FYcStorages.setWalletInfo(
+//           FYcEntitysWallet.fromJson(apisBaseResponse.data['walletInfo'] ?? {}));
+//       FYcEventBus.instance.fire(FYcEntitysEventsUserInfoUpdate());
+//       FYcEventBus.instance.fire(FYcEntitysEventsBehaviorUpdate());
+//       FYcEventBus.instance.fire(FYcEntitysEventsWalletUpdate());
+//     }
+//     return apisBaseResponse.data;
+//   }
+
+//   static Future<void> logout() async {
+//     FYcApisBaseResponse apisBaseResponse = await FYcApisDio.instance
+//         .post('/api/default/pub_remoteConfig.query', params: {}, tips: true);
+//     if (apisBaseResponse.success) {
+//       FYcStorages.cleanAllLoginInfo();
+//       FYcEventBus.instance.fire(FYcEntitysEventsUserInfoUpdate());
+//       FYcEventBus.instance.fire(FYcEntitysEventsBehaviorUpdate());
+//       FYcEventBus.instance.fire(FYcEntitysEventsWalletUpdate());
+//     }
+//   }
+
+//   static Future<void> queryRemoteConfig() async {
+//     int timestamp = FYcStorages.lastRemoteConfigTimestamp();
+//     if (DateTime.now().millisecondsSinceEpoch - timestamp < 1000 * 60 * 60) {
+//       return;
+//     }
+//     FYcStorages.setLastRemoteConfigTimestamp();
+//     FYcApisBaseResponse apisBaseResponse = await FYcApisDio.instance
+//         .post('/api/default/pub_remoteConfig.query', params: {}, tips: true);
+//     if (apisBaseResponse.success) {
+//       FYcEntitysRemoteConfig entitysRemoteConfig =
+//           FYcEntitysRemoteConfig.fromJson(apisBaseResponse.data);
+//       FYcStorages.setRemoteConfig(entitysRemoteConfig);
+//     }
+//   }
+
+  static Future<void> getWalletInfo() async {
+    FYcApisBaseResponse apisBaseResponse = await FYcApisDio.instance
+        .post('/api/default/pub_remoteConfig.query', params: {}, tips: true);
+    if (apisBaseResponse.success) {
+      Map<String, dynamic> walletInfo = apisBaseResponse.data['walletInfo'];
+      if (walletInfo.isNotEmpty) {
+        FYcEntitysWallet entitysWallet = FYcEntitysWallet.fromJson(walletInfo);
+        FYcStorages.setWalletInfo(entitysWallet);
+        FYcEventBus.instance.fire(FYcEntitysEventsWalletUpdate());
+      }
+    }
+  }
+
+//   static Future<void> getBehaviorInfo() async {
+//     FYcApisBaseResponse apisBaseResponse = await FYcApisDio.instance
+//         .post('/api/default/pub_remoteConfig.query', params: {}, tips: true);
+//     if (apisBaseResponse.success) {
+//       Map<String, dynamic> behaviorInfo = apisBaseResponse.data['behaviorInfo'];
+//       if (behaviorInfo.isNotEmpty) {
+//         FYcEntitysBehavior entitysBehavior =
+//             FYcEntitysBehavior.fromJson(behaviorInfo);
+//         FYcStorages.setBehaviorInfo(entitysBehavior);
+//         FYcEventBus.instance.fire(FYcEntitysEventsBehaviorUpdate());
+//       }
+//     }
+//   }
+
+//   static Future<bool> submitCashOut(int amount) async {
+//     if (amount > 0) {
+//       FYcApisBaseResponse apisBaseResponse = await FYcApisDio.instance.post(
+//           '/api/default/pub_remoteConfig.query',
+//           params: {amount: amount.toString()},
+//           tips: true);
+//       if (apisBaseResponse.success) {
+//         Map<String, dynamic> walletInfo = apisBaseResponse.data['walletInfo'];
+//         if (walletInfo.isNotEmpty) {
+//           FYcEntitysWallet entitysWallet =
+//               FYcEntitysWallet.fromJson(walletInfo);
+//           FYcStorages.setWalletInfo(entitysWallet);
+//           FYcEventBus.instance.fire(FYcEntitysEventsWalletUpdate());
+//         }
+//         Map<String, dynamic> behaviorInfo =
+//             apisBaseResponse.data['behaviorInfo'];
+//         if (behaviorInfo.isNotEmpty) {
+//           FYcEntitysBehavior entitysBehavior =
+//               FYcEntitysBehavior.fromJson(behaviorInfo);
+//           FYcStorages.setBehaviorInfo(entitysBehavior);
+//           FYcEventBus.instance.fire(FYcEntitysEventsBehaviorUpdate());
+//         }
+//         return true;
+//       }
+//     }
+//     return false;
+//   }
+
+//   static Future<List> queryWalletLog() async {
+//     FYcApisBaseResponse apisBaseResponse = await FYcApisDio.instance
+//         .post('/api/default/pub_remoteConfig.query', params: {}, tips: true);
+//     if (apisBaseResponse.success) {
+//       return apisBaseResponse.data;
+//     }
+//     return [];
+//   }
+
+//   static Future<List> queryCashOuts() async {
+//     FYcApisBaseResponse apisBaseResponse = await FYcApisDio.instance
+//         .post('/api/default/pub_remoteConfig.query', params: {}, tips: true);
+//     if (apisBaseResponse.success) {
+//       return apisBaseResponse.data;
+//     }
+//     return [];
+//   }
+
+  static Future<void> reportAppPraise() async {
+    FYcApisBaseResponse apisBaseResponse = await FYcApisDio.instance
+        .post('/api/default/pub_remoteConfig.query', params: {}, tips: true);
+    if (apisBaseResponse.success) {
+      Map<String, dynamic> behaviorInfo = apisBaseResponse.data['behaviorInfo'];
+      if (behaviorInfo.isNotEmpty) {
+        FYcEntitysBehavior entitysBehavior =
+            FYcEntitysBehavior.fromJson(behaviorInfo);
+        FYcStorages.setBehaviorInfo(entitysBehavior);
+        FYcEventBus.instance.fire(FYcEntitysEventsBehaviorUpdate());
+      }
+    }
+  }
+
+//   static Future<void> reportAdClick(
+//       {String slotID = '', String adnName = '', String adnSlotID = ''}) async {
+//     Future.delayed(const Duration(seconds: 5), () async {
+//       FYcApisBaseResponse apisBaseResponse = await FYcApisDio.instance.post(
+//           '/api/default/pub_remoteConfig.query',
+//           params: {slotID: slotID, adnName: adnName, adnSlotID: adnSlotID});
+//       if (apisBaseResponse.success) {
+//         bool isReward = apisBaseResponse.data['isReward'];
+//         if (isReward && (await Vibration.hasVibrator() ?? false)) {
+//           Vibration.vibrate();
+//         }
+//       }
+//     });
+//   }
+
+//   static Future<List> queryUserWelfareRe() async {
+//     FYcApisBaseResponse apisBaseResponse = await FYcApisDio.instance
+//         .post('/api/default/pub_remoteConfig.query', params: {}, tips: true);
+//     if (apisBaseResponse.success) {
+//       return apisBaseResponse.data;
+//     }
+//     return [];
+//   }
+
+//   static Future<bool> receiveUserWelfareRe(int amount, String code) async {
+//     FYcApisBaseResponse apisBaseResponse = await FYcApisDio.instance.post(
+//         '/api/default/pub_remoteConfig.query',
+//         params: {amount: amount.toString(), code: code},
+//         tips: true);
+//     if (apisBaseResponse.success) {
+//       Map<String, dynamic> walletInfo = apisBaseResponse.data['walletInfo'];
+//       if (walletInfo.isNotEmpty) {
+//         FYcEntitysWallet entitysWallet = FYcEntitysWallet.fromJson(walletInfo);
+//         FYcStorages.setWalletInfo(entitysWallet);
+//         FYcEventBus.instance.fire(FYcEntitysEventsWalletUpdate());
+//       }
+//       return true;
+//     }
+//     return false;
+//   }
+
+//   static Future<int> submitSign() async {
+//     FYcApisBaseResponse apisBaseResponse = await FYcApisDio.instance
+//         .post('/api/default/pub_remoteConfig.query', params: {}, tips: true);
+//     if (apisBaseResponse.success) {
+//       int amount = apisBaseResponse.data['amount'];
+//       Map<String, dynamic> walletInfo = apisBaseResponse.data['walletInfo'];
+//       if (walletInfo.isNotEmpty) {
+//         FYcEntitysWallet entitysWallet = FYcEntitysWallet.fromJson(walletInfo);
+//         FYcStorages.setWalletInfo(entitysWallet);
+//         FYcEventBus.instance.fire(FYcEntitysEventsWalletUpdate());
+//       }
+//       Map<String, dynamic> behaviorInfo = apisBaseResponse.data['behaviorInfo'];
+//       if (behaviorInfo.isNotEmpty) {
+//         FYcEntitysBehavior entitysBehavior =
+//             FYcEntitysBehavior.fromJson(behaviorInfo);
+//         FYcStorages.setBehaviorInfo(entitysBehavior);
+//         FYcEventBus.instance.fire(FYcEntitysEventsBehaviorUpdate());
+//       }
+//       return amount;
+//     }
+//     return 0;
+//   }
+
+//   static Future<int> receiveContinuitySign(int days) async {
+//     FYcApisBaseResponse apisBaseResponse = await FYcApisDio.instance.post(
+//         '/api/default/pub_remoteConfig.query',
+//         params: {days: days.toString()},
+//         tips: true);
+//     if (apisBaseResponse.success) {
+//       int amount = apisBaseResponse.data['amount'];
+//       Map<String, dynamic> walletInfo = apisBaseResponse.data['walletInfo'];
+//       if (walletInfo.isNotEmpty) {
+//         FYcEntitysWallet entitysWallet = FYcEntitysWallet.fromJson(walletInfo);
+//         FYcStorages.setWalletInfo(entitysWallet);
+//         FYcEventBus.instance.fire(FYcEntitysEventsWalletUpdate());
+//       }
+//       Map<String, dynamic> behaviorInfo = apisBaseResponse.data['behaviorInfo'];
+//       if (behaviorInfo.isNotEmpty) {
+//         FYcEntitysBehavior entitysBehavior =
+//             FYcEntitysBehavior.fromJson(behaviorInfo);
+//         FYcStorages.setBehaviorInfo(entitysBehavior);
+//         FYcEventBus.instance.fire(FYcEntitysEventsBehaviorUpdate());
+//       }
+//       return amount;
+//     }
+//     return 0;
+//   }
+}
