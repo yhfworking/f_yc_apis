70ad4fd1d5470559915865944a8a55fac5f0b94c
yhfwork <mm20102010mm>
2022-11-10 15:12:15 +0800

    Update f_yc_apis_default.dart

--- lib/src/f_yc_apis_default.dart
+++ lib/src/f_yc_apis_default.dart
@@ -30,8 +30,10 @@ class FYcApisDefault {
 
   static Future<Map<String, dynamic>?> wxLogin(String code,
       {String inviteCode = ''}) async {
-    FYcApisBaseResponse apisBaseResponse = await FYcApisDio.instance
-        .post('/api/pub_auth.wxLogin', params: {code, inviteCode}, tips: true);
+    FYcApisBaseResponse apisBaseResponse = await FYcApisDio.instance.post(
+        '/api/pub_auth.wxLogin',
+        params: {code: code, inviteCode: inviteCode},
+        tips: true);
     if (apisBaseResponse.success) {
       FYcStorages.setUserToken(apisBaseResponse.data['unitoken']);
       FYcStorages.setUserTokenExpired(apisBaseResponse.data['tokenExpired']);
